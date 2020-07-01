9ca23674d5565ba2d4c0d535d292db82e5063b1b
Max Federlick <maxfederlick@Maxs-MBP.lan>
2020-07-01 09:18:28 -0600

    New api used, all data rendering on page

--- stock-app/Scrap.js
+++ stock-app/Scrap.js
@@ -6,4 +6,7 @@ TTY3VLM113GIDNC8
 
 {Object.keys(stockDisplay).map((symbol, i) => (
   <h2 key={i}>Ticker :{stockDisplay[symbol]}</h2>
-))}
+))
+}
+
+pk_bd42dc5ddd804573b2e313f99b21a0a4 
--- stock-app/src/Stock.js
+++ stock-app/src/Stock.js
@@ -6,32 +6,45 @@ import axios from 'axios'
   
 function Stock(props) {
 
-  let [stockDisplay, changeStockDisplay] = useState({})
+  let [stockMetrics, changeStockMetrics] = useState([])
+  let [stockNews, changeStockNews] = useState([])
+  let [stockNews2, changeStockNews2] = useState([])
 
   useEffect(() => {
 
     const apiCall = async () => {
-      const data = await axios(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${props.name}&apikey=TTY3VLM113GIDNC8`,
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-      console.log(data.data["Global Quote"])
-      changeStockDisplay(data.data["Global Quote"])
+      const data = await axios(`https://cloud.iexapis.com/stable/stock/aapl/batch?types=quote,news,chart&range=1m&last=10&token=pk_bd42dc5ddd804573b2e313f99b21a0a4 `)
+      console.log(data.data)
+      changeStockMetrics(data.data.quote)
+      const mappedObjectNews = (data.data.news[0])
+      const mappedObjectNews2 = (data.data.news[1])
+      console.log(mappedObjectNews)
+      changeStockNews(mappedObjectNews)
+      changeStockNews2(mappedObjectNews2)
       }
       apiCall()
   
     }, [])
 
+ 
   
     return (
       <>
-        <div>
-       {Object.keys(stockDisplay).map((symbol, i) => (
-    <h2 key={i}>Ticker : {stockDisplay[symbol]}</h2>
-  ))}
-        </div>
+        
+        <h2>{stockMetrics.companyName}</h2>
+        <h3>Real Time Price ({stockMetrics.latestTime}): ${stockMetrics.iexRealtimePrice}</h3>
+        <h3>Change: ${stockMetrics.change} ({stockMetrics.changePercent}%)</h3>
+        <h3>Previous Close: ${stockMetrics.previousClose}</h3>
+        <h3>52 Week High: {stockMetrics.week52High}</h3>
+        <h3>52 Week High: {stockMetrics.week52Low}</h3>
+        
+        <h4>{(stockNews.headline)}</h4>
+        <h5>{(stockNews.summary)}</h5>
+        <img src={stockNews.image} />
+        <h4>{(stockNews2.headline)}</h4>
+        <h5>{(stockNews2.summary)}</h5>
+        <img src={stockNews2.image} />
+      
       </>
     )
 
